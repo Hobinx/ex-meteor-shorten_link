fa72836e37f35c5e6dbae22d35e1b5620c30ae11
Hobinx <binservmail@gmail.com>
2017-11-22 23:42:28 +0800

    Header component

---
+++ client/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="nav navbar-default">
+      <div className="nav-header">
+        <a className="navbar-brand">Shorten Link</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
