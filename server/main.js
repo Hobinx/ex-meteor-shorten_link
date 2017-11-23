986924df02bd1d11bce22d3e235c2fdde3a8b0fe
Hobinx <binservmail@gmail.com>
2017-11-23 10:33:36 +0800

    redirect user with correct token

--- server/main.js
+++ server/main.js
@@ -1,8 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
     return Links.find({});
   });
 });
+
+function redirectUser(req, res, next) {
+  const link = Links.findOne({ token: req.params.token });
+
+  if (link) {
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+  } else {
+    next();
+  }
+}
+
+const middleware = ConnectRoute(router => {
+  router.get('/:token', redirectUser);
+})
+
+WebApp.connectHandlers.use(middleware);
