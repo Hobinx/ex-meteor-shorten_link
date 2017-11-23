3aba57adfddfd5f5cdc7753dd3c1d8eeacdfd0a5
Hobinx <binservmail@gmail.com>
2017-11-23 09:20:05 +0800

    add validate url as meteor method for LinkCreate component

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: '' };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    Meteor.call('links.insert', this.linkInput.value, (error) => {
+      if (error) {
+        this.setState({ error: 'Enter a valid URL' });
+      } else {
+        this.setState({ error: '' });
+        this.linkInput.value = '';
+      }
+    });
+  }
+
   render() {
     return (
-      <form action="">
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control"
+                 ref={input => this.linkInput = input} />
         </div>
+        <div className="text-danger">{this.state.error}</div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
     );
   }
 }
+
 
 export default LinkCreate;
